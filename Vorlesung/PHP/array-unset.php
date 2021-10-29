8ec1b284a2ee8676f1d8cf82cb0b31754958718b
prof_riemke <riemke-gurzki@hdm-stuttgart.de>
2021-10-29 10:40:54 +0200

    Woche 3

---
+++ Vorlesung/PHP/array-unset.php
@@ -0,0 +1,5 @@
+<?php
+$a = array("Man", "Woman", "Camera");
+var_dump($a);
+unset($a[1]);
+var_dump($a);
---
+++ Vorlesung/PHP/assoziat-array.php
@@ -0,0 +1,8 @@
+<?php
+
+$a=array(
+ "name"=> "Meier",
+    "vorname"=>"Tanja",
+    "alter"=>24
+);
+echo "Hallo ".$a["vorname"]." Du bist ".$a["alter"];
